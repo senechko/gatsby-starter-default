d41b6278bf6548068552f8039697b17d0be5723a
Paul Senechko <senechko@yahoo.com>
2019-09-26 13:22:50 -0700

    add parsing of name

--- src/components/form.js
+++ src/components/form.js
@@ -4,14 +4,25 @@ const searchParams =
   typeof window !== "undefined"
     ? new URLSearchParams(window.location.search)
     : null;
+
+let appName = "Random-" + new Date().getTime();
 let name = "";
+
 if (searchParams) {
   name = searchParams.get("name");
+  var lastSlash = name.lastIndexOf("/");
+  if (lastSlash != -1) {
+    appName = name.substr(
+      lastSlash + 1,
+      name.lastIndexOf(".") - (lastSlash + 1)
+    );
+  }
 }
 
 export default class Form extends React.Component {
   state = {
     name: name,
+    appName: appName,
   };
 
   handleSubmit = event => {
@@ -31,20 +42,23 @@ export default class Form extends React.Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit} action="/deploy">
-        <div>
-          <label>Site name</label>
-          <input
-            type="text"
-            name="name"
-            onChange={this.handleInputChange}
-            value={this.state.name}
-            width="60"
-          />
-          <span>.westegg.io</span>
-        </div>
-        <input type="image" src="/button_deploy-to-digitalocean.png" />
-      </form>
+      <>
+        <div>Deploying: {this.state.name}</div>
+        <form onSubmit={this.handleSubmit} action="/deploy">
+          <div>
+            <label>Site name</label>
+            <input
+              type="text"
+              name="name"
+              onChange={this.handleInputChange}
+              value={this.state.appName}
+              width="60"
+            />
+            <span>.westegg.io</span>
+          </div>
+          <input type="image" src="/button_deploy-to-digitalocean.png" />
+        </form>
+      </>
     );
   }
 }
