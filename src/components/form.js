152c58cc7ded105d5b3b97949b8964854739400d
Paul Senechko <senechko@yahoo.com>
2019-09-27 10:33:41 -0700

    rm api prefix

--- src/components/form.js
+++ src/components/form.js
@@ -47,7 +47,7 @@ export default class Form extends React.Component {
         <form
           onSubmit={this.handleSubmit}
           method="POST"
-          action="https://api.westegg.io/sites"
+          action="https://westegg.io/sites"
         >
           <input type="hidden" name="repo_url" value={this.state.repoUrl} />
           <div>
